507bfa05d2d2f3a573348f03620086d30c3af970
Poonam <poonamsodani09@gmail.com>
2018-04-04 22:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key){
+  var newobject = Object.assign({},object)
+  delete newobject[key]
+  return newobject
+}
+
+function
